c5345998d80a45e442afa6f379b74b66264541fc
Neeraj Tirumalasetty <neerajtirumalasetty14@gmail.com>
2026-01-15 18:31:16 +0530

    URL Update

--- Frontend/src/components/FreeBooks.jsx
+++ Frontend/src/components/FreeBooks.jsx
@@ -11,7 +11,7 @@ function FreeBooks() {
   useEffect(()=>{
     const getBook = async()=>{
       try {
-      const res  = await axios.get("http://localhost:4001/book")
+      const res  = await axios.get("https://bookstore-gr4z.onrender.com/book")
       console.log(res.data)
       setBook(res.data.filter((data) => data.category === "Free"))
       } catch (error) {
